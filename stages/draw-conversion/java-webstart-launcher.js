2e85e78fe8e4b0d6a85437d60accd82d11d87fbc
Anders Riutta <git@andersriutta.com>
2016-10-17 15:45:36 -0700

    Rearrange buttons

--- stages/draw-conversion/java-webstart-launcher.js
+++ stages/draw-conversion/java-webstart-launcher.js
@@ -10,7 +10,9 @@ $(function() {
   var dir = location.href.substring(0, location.href.lastIndexOf('/')+1);
   var url = dir + '/WP4v88153.jnlp';
 
-  deployJava.createWebStartLaunchButton(url, '1.6.0');
+  // NOTE: this line automatically creates a launch button, but
+  // it replaces the rest of the content on the page:
+  //deployJava.createWebStartLaunchButton(url, '1.6.0');
 
   $('#edit').click(function() {
     deployJava.launchWebStartApplication(url);
