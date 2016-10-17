1e159a7fff45c9a8208ceca55a06d76cff32ea3b
Anders Riutta <git@andersriutta.com>
2016-10-17 15:42:11 -0700

    Add edit button.

--- stages/draw-conversion/java-webstart-launcher.js
+++ stages/draw-conversion/java-webstart-launcher.js
@@ -1,13 +1,18 @@
+// Referencing documentation from
+// https://docs.oracle.com/javase/tutorial/deployment/webstart/deploying.html
+//
+// Using JavaScript to get location of JNLP
+// file relative to HTML page
+// NOTE: may be neccesary to change the url, depending on how the JNLP file
+// named and where it is located.
+
 $(function() {
+  var dir = location.href.substring(0, location.href.lastIndexOf('/')+1);
+  var url = dir + '/WP4v88153.jnlp';
+
+  deployJava.launchWebStartApplication(url);
+
   $('#edit').click(function() {
-    // from https://docs.oracle.com/javase/tutorial/deployment/webstart/deploying.html
-    // using JavaScript to get location of JNLP
-    // file relative to HTML page
-    // NOTE: may be neccesary to change the url, depending on how the JNLP generator
-    // is made available as an extension.
-    var dir = location.href.substring(0, location.href.lastIndexOf('/')+1);
-    var url = dir + '/WP4v88153.jnlp';
-    //deployJava.launchWebStartApplication(url);
     deployJava.createWebStartLaunchButton(url, '1.6.0');
   });
 });
