04be67f5164db5bbc43b810430fcc08a239dc4f1
Alexander Pico <apico@gladstone.ucsf.edu>
2016-11-05 11:42:10 -0700

    first yellow challenge and scripts

---
+++ javascripts/choice.js
@@ -0,0 +1,27 @@
+$(document).ready(
+function(){
+	$('.choices a').click(function(){
+	   if($(this).prop('href')){
+		$('.explanations').hide();
+		$(this).find('span').removeClass('fa-circle-thin');
+		if($(this).hasClass('true')){
+			$(this).find('span').addClass('correct')
+                        $(this).find('span').addClass('fa-check-circle');
+			showExplanation('ex1');
+		} else {
+			$(this).find('span').addClass('wrong');
+			$(this).find('span').addClass('fa-times-circle');
+			showExplanation('ex2');
+		}
+		$(this).removeAttr('href');
+		$(this).addClass('selected');
+	  }
+	});
+
+function showExplanation(a){
+	$('#'+a).slideToggle('slow');
+	}
+
+});
+
+
