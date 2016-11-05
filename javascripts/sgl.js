57332fb577aae2ded67f0d2dbb20d2fdab3e18b6
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2016-11-05 10:36:19 -0700

    Added iframe check

--- javascripts/sgl.js
+++ javascripts/sgl.js
@@ -1,42 +1,44 @@
 window.$(window).load(function() {
-    var token;
-    var myParam = location.search.split('token=')[1];
-    if (typeof myParam !== 'undefined' && myParam !== null) {
-      token = myParam.split('&')[0];
-    } else {
-      console.error('ScienceGameLab Error: you are not logged in with a valid token');
-    }
+    if ( window.location !== window.parent.location ) {
+        var token;
+        var myParam = location.search.split('token=')[1];
+        if (typeof myParam !== 'undefined' && myParam !== null) {
+            token = myParam.split('&')[0];
+        } else {
+            console.error('ScienceGameLab Error: you are not logged in with a valid token');
+        }
 
-    var game = {
-      gameId: '5751ed90e4b050b536ba7a03',
-      token: token,
-    };
+        var game = {
+        gameId: '5751ed90e4b050b536ba7a03',
+        token: token,
+        };
 
-    window.wpSGL = {
-      postLeaderboard: function(score, cb) {
-        var game_data = {
-          _t:'msg',
-          body:{
+        window.wpSGL = {
+        postLeaderboard: function(score, cb) {
+            var game_data = {
+            _t:'msg',
+            body:{
             _t:'leaderboard.post',
             accounttoken: game.token,
             gametoken: game.gameId,
             gamescore: '' + score
-          },
-          header: { _t: 'hdr', cid: ('' + window.SGL.sessioncounter) }
-        };
-        var post_data = JSON.stringify(game_data);
-        window.SGL.post_leaderboard(post_data, cb);
-      },
-      submitSGLActivity: function(action, cb) {
-        var activity_body = {
+            },
+            header: { _t: 'hdr', cid: ('' + window.SGL.sessioncounter) }
+            };
+            
+            var post_data = JSON.stringify(game_data);
+            window.SGL.post_leaderboard(post_data, cb);
+        },
+        submitSGLActivity: function(action, cb) {
+            var activity_body = {
             _t: 'activity.create',
             accounttoken: game.token,
             details: { gameId: game.gameId },
             name: action,
             time: (new Date()).toTimeString()
+            };
+            window.SGL.create_activity(activity_body, cb);
+        },
         };
-        window.SGL.create_activity(activity_body, cb);
-      },
-    };
-    
+    }
 });
