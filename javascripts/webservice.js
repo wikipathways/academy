0f42648d7a03f58e842914c3b11b51e29209574f
Alexander Pico <apico@gladstone.ucsf.edu>
2016-11-06 13:59:38 -0800

    new webservice stage

--- javascripts/webservice.js
+++ javascripts/webservice.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('button').click( function() {
+  $('#wp-account-button').click( function() {
 	var form = document.getElementById('wp-account');
 	var username= form.getElementsByTagName('input').username.value;
 	var password= form.getElementsByTagName('input').password.value;
@@ -22,6 +22,31 @@ $(document).ready(function(){
 		}
 	});
   });
+  $('#wp-add-ontology-button').click( function() {
+	console.log('posting to web service');
+	$('.results').hide();
+	var timestamp = getAnHourAgo();
+	console.log(timestamp);
+	$.ajax({
+		type: 'GET',
+		url: 'http://webservice.wikipathways.org/getPathwayHistory?pwId=WP4&timestamp='+timestamp+'&format=xml',
+		dataType: 'xml',
+		success: function (response) {
+			console.log(response);
+			//var t = $(response).children().text();
+			 var tag = $(response).find('ns2\\:comment').text(); 
+			 var user = $(response).find('ns2\\:user').text();
+			console.log(tag);
+			console.log(user);
+			showResult('success');
+			sendSGLActivity('wikipathways-103');
+		},
+		error: function (error) {
+			console.log(error);
+			showResult('error');
+		}
+	});
+  });
 
   function showResult(result){
 		$('#'+result).slideToggle('slow', function(){
@@ -36,4 +61,27 @@ $(document).ready(function(){
 	  console.log(action);
 
   }
+  
+  function getAnHourAgo(){
+	var d = new Date(Date.now());
+	var YYYY = d.getFullYear();
+	var MM = forceDoubleDigit(d.getMonth()) + 1; //only month is zero-based 
+	var DD = forceDoubleDigit(d.getDate());
+	var hh = d.getHours();
+		if(hh>0){
+			forceDoubleDigit(hh-1); // an hour ago
+		} else {
+			hh = '23'; //an hour ago, yesterday
+			DD = forceDoubleDigit(DD-1); // I think '00' day actually works in wp webservice...
+		}
+	var mm = forceDoubleDigit(d.getMinutes());
+        var ss = forceDoubleDigit(d.getSeconds());
+	return(""+YYYY+MM+DD+hh+mm+ss);	
+  }
+  function forceDoubleDigit(x){
+	if(x<10)
+		x='0'+x;
+	return(x);
+  }
+
 });
