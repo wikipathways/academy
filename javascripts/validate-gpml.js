e7aa3bd9761e5824dbaacc2339b0f9c56f0c403d
Alexander Pico <apico@gladstone.ucsf.edu>
2016-12-02 18:14:44 -0800

    major overhaul of gpml validation

--- javascripts/validate-gpml.js
+++ javascripts/validate-gpml.js
@@ -51,6 +51,12 @@ window.wpAcademy.validateGPML = function(args) {
     status.style.visibility = 'visible';
   });
 
+  var solutionGpml = '';
+  $.ajax({url:validate,success:function(s){
+    solutionGpml = s;
+  }
+  });
+
   // file-dragger.js is a browserified version of
   // https://www.npmjs.com/package/file-dragger
   // with FileDragger added to window.
@@ -59,8 +65,8 @@ window.wpAcademy.validateGPML = function(args) {
     var reader = new FileReader();
     reader.onload = function(evt) {
       var srcElement = evt.hasOwnProperty('srcElement') ? evt.srcElement : evt.target;
-      var actualStr = srcElement.result;
-      var passes = validate(actualStr);
+      var userGpml = srcElement.result;
+      var passes = validateGpml(userGpml,solutionGpml);
       status.style.visibility = 'visible';
       status.style.fontSize = '20px';
       status.style.color = 'red';
@@ -95,3 +101,96 @@ window.wpAcademy.validateGPML = function(args) {
     reader.readAsText(file);
   });
 };
+
+function parseGpml(gpml){
+          //console.log(gpml);
+
+          // DataNode collection
+          var data = {};
+          $(gpml).find('DataNode').each(function(){
+                  var gi = $(this).attr('GraphId');
+                  var tl = $(this).attr('TextLabel').toUpperCase();
+                  var ty = $(this).attr('Type');
+                  var xdb = $(this).find('Xref').attr('Database');
+                  var xid = $(this).find('Xref').attr('ID');
+                  data[gi] = [tl,ty,xdb,xid,[]]; //insert array placeholder for interaction collection
+          });
+
+          // Group collection
+          $(gpml).find('Group').each(function(){
+                  var gi = $(this).attr('GraphId');
+                  data[gi] = ['GROUP','Group','NULL','NULL',[]];
+          });
+
+          // Anchor collection
+          $(gpml).find('Interaction').each(function(){
+                  $(this).find('Graphics').find('Anchor').each(function(){
+                        var gi = $(this).attr('GraphId');
+                        data[gi] = ['ANCHOR','Anchor','NULL','NULL',[]];
+                  });
+          });
+
+          // Interaction collection
+          $(gpml).find('Interaction').each(function(){
+                  $(this).find('Graphics').find('Point').each(function() {
+                          var gr = $(this).attr('GraphRef');
+                          var ah = $(this).attr('ArrowHead');
+                          if (undefined === ah) { ah = 'Line';}
+                          if (undefined === data[gr]){
+                                console.log('GraphRef pointing to missing GraphId: '+gr);
+                          } else {
+                                data[gr][4].push(ah);
+                          }
+                  });
+          });
+          console.log(data);
+
+	  return data;
+}
+function validateGpml(userGpml,solutionGpml){
+	 var userData = parseGpml(userGpml);
+ 	 var solutionData = parseGpml(solutionGpml);
+
+          var err = '';
+	  userDataCount = Object.keys(userData).length;
+          solutionDataCount = Object.keys(solutionData).length;
+          err += (userDataCount == solutionDataCount) ? '' : 'Incorrect number of nodes: '+userDataCount+' detected. ';
+
+          var userlabels = [];
+          $.each(userData, function(userkey, userval){
+                userlabels.push(userval[0]);
+                err += (userval[2] != '') ? '' : 'Missing Xref database for: '+userval[0]+'. ';
+                err += (userval[3] != '') ? '' : 'Missing Xref identifier for: '+userval[0]+'. ';
+          });
+
+          $.each(solutionData, function(solkey, solval){
+                err += (userlabels.includes(solval[0])) ? '' : 'Missing '+solval[0]+' node. ';
+                var intmatch = true;
+                var typematch = true;
+                $.each(userData, function(userkey, userval){
+                        if (solval[0] == userval[0]){
+                          intmatch = false;
+                          typematch = false;
+                          if (solval[1] == userval[1]){
+                                typematch = true;
+                          }
+                          if ($(solval[4]).not(userval[4]).length === 0 && $(userval[4]).not(solval[4]).length === 0){
+                                intmatch = true;
+                          }
+                        }
+                });
+                err += (typematch) ? '' : 'Incorrect molecule type for '+solval[0]+'. ';
+                err += (intmatch) ? '' : 'Incorrect interactions for '+solval[0]+'. ';
+
+          });
+
+          if (err.length == 0){
+                  console.log('GPML is valid');
+                  return true;
+          }else {
+                  console.log('GPML errors: '+err);
+                  return false
+          }
+     
+}
+
