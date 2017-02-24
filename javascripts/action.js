a0acb437b2c0e80711b63d7ece4eac1689c573f1
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2017-02-24 14:56:25 -0800

    Create action.js

---
+++ javascripts/action.js
@@ -0,0 +1,18 @@
+$(document).ready(
+function(){
+
+  action = ($('.action').prop('id'));
+
+	console.log(action);	
+  if ( window.location !== window.parent.location ) {
+    window.wpSGL.submitSGLActivity(action, function(err, response) {
+    console.log('SGL submit activity');
+	  if (err) {
+			console.log('err');
+  		console.log(err);
+  	}
+  	console.log('response');
+  	console.log(response);
+	  });
+  }
+});
